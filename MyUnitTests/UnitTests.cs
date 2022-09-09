8f3d589caceea71b7993659fa2254ae7aac44a73
Aaron Marcus <aaron.lee.marcus@gmail.com>
2022-09-09 15:14:23 -0400

    Adding more unit tests

--- MyUnitTests/UnitTests.cs
+++ MyUnitTests/UnitTests.cs
@@ -4,13 +4,14 @@ namespace MyUnitTests;
 
 public class UnitTests
 {
-    int add(int x, int y) => (x+y);
+    private static int add(int x, int y) => (x+y);
 
     [Theory]
     [InlineData(1, 2, 3)]
     [InlineData(-4, -6, -10)]
     [InlineData(-2, 2, 0)]
     [InlineData(int.MinValue, -1, int.MaxValue)]
+    [InlineData(0,0,0)]
     public void CanAddTheory(int value1, int value2, int expected)
     {
         Assert.Equal(expected, add(value1,value2));
